6922247e84d08df614adfa15894d33be62c0e8d6
Lucas Medeiros da Silva <med_lucassilva@hotmail.com>
2024-04-18 14:26:19 -0300

    Configuração gettext

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.views.generic import FormView
 from .models import Servico, Funcionario, Funcao
 from .forms import ContatoForm
+from django.utils.translation import gettext as _
+from django.utils import translation
 
 
 class IndexView(FormView):
@@ -14,17 +16,20 @@ class IndexView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        lang = translation.get_language()
         context['servicos'] = Servico.objects.order_by('?').all()
         context['funcionario'] = Funcionario.objects.order_by('?').all()
         context['funcoes'] = list(Funcao.objects.all())
+        context['lang'] = lang
+        translation.activate(lang)
         return context
 
     def form_valid(self, form, *args, **kwargs):
         form.send_mail()
-        messages.success(self.request, 'E-mail enviado com sucesso!')
+        messages.success(self.request, _('E-mail enviado com sucesso!'))
         return super(IndexView, self).form_valid(form, *args, **kwargs)
     
     def form_invalid(self, form, *args, **kwargs):
-        messages.error(self.request, 'Erro ao enviar o e-mail')
+        messages.error(self.request, _('Erro ao enviar o e-mail'))
         return super(IndexView, self).form_invalid(form, *args, **kwargs)
 
--- fusion/settings.py
+++ fusion/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,3 +148,8 @@ EMAIL_USE_TSL = True
 EMAIL_HOST_PASSWORD = 'fusion'
 DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 """
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR,'locale'),
+)
+
